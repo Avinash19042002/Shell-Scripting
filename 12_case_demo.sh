1bbb24b9c8d07608a78abbe9ee92a450ccb7f285
Ubuntu <ubuntu@ip-172-31-43-156.ap-south-1.compute.internal>
2024-05-22 10:45:18 +0000

    Logical Operators

--- 12_case_demo.sh
+++ 12_case_demo.sh
@@ -5,10 +5,14 @@ echo "a for print date"
 echo "b for list of scripts"
 echo "c to check the current location"
 
-read -p "Enter your Choice" choice
+read -p "Enter your Choice : " choice 
 
 case $choice in 
-	a)date;;
+	a)
+		echo "Today's date is :"
+		date
+		echo "Have a Nice Day!!"
+		;;
 	b)ls;;
 	c)pwd;;
 	*)echo "Not a valid Choice"
