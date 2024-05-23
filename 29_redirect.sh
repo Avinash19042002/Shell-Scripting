3a428da947f8475b2349b6bc4b58d7d71a3a2c42
Ubuntu <ubuntu@ip-172-31-43-156.ap-south-1.compute.internal>
2024-05-23 14:37:31 +0000

    Redirection in shell

--- 29_redirect.sh
+++ 29_redirect.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# > -> overwrite the result into the file
+# >> -> Append the result to the end of the file
+
 ping -c 1 www.google.com > redirect.log
 
+ping -c 1 www.facebook.com >> redirect.log
