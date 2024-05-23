a723cc849ace9ca7c5135641b7cfb6e1e8b68740
Ubuntu <ubuntu@ip-172-31-43-156.ap-south-1.compute.internal>
2024-05-23 10:47:10 +0000

    Largest file in current directory

--- findLargestFile.sh
+++ findLargestFile.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+echo -e "please Specify whether you want to search irrespective of a specific extension or \nwith a extension"
+
+echo -e "\n"
+echo "Enter a -> with a Extension like txt"
+echo "Enter b -> without an Extension"
+
+read -p "Enter you choice : " choice
+
+# Initialize variables
+largest_size=0
+largest_file=""
+
+case $choice in
+	a)
 # Asking the User to provide the extension 
 read -p "Enter the Extension of the files you are interested in : " exten
+
+
 
 # Check if there are no files with the specified extension in the current directory
 if [ ! "$(ls *.$exten 2>/dev/null)" ]; then
     echo "No files with .$exten extension found in the current directory"
     exit 1
 fi
-
-# Initialize variables
-largest_size=0
-largest_file=""
 
 # Loop through files in the current directory
 for file in *
@@ -30,7 +42,35 @@ do
         fi
     fi
 done
+;;
 
+b)
+	# Check if any files are present in the current directory
+if [ ! "$(ls -A)" ]; then
+    echo "No files found in the current directory"
+    exit 1
+fi
+
+# Loop through files in the current directory
+for file in *
+do
+    # Check if the file is a regular file
+    if [ -f "$file" ]; then
+        # Get the size of the file
+        size=$(stat -c %s "$file")
+
+        # Compare the size with the current largest size
+        if [ $size -gt $largest_size ]; then
+            largest_size=$size
+            largest_file=$file
+        fi
+    fi
+done
+;;
+
+*) 
+	echo "Invalid Choice!!"
+esac
 # Check if any files were found
 if [ -z "$largest_file" ]; then
     echo "No files found in the current directory"
